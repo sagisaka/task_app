fc44b0e6bc601702f5e91b4e72fb2a82727ab476
sagisaka2 <sagisaka2>
2017-06-12 00:52:24 +0900

    インデントの修正を行った

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,56 +1,56 @@
 class ProjectsController < ApplicationController
 
 
-    def index
-        @search = Project.search(params[:q])
-        if @search.result == "null"
-            @projects = Project.all
-        else
-            @projects = @search.result
-        end
+  def index
+    @search = Project.search(params[:q])
+    if @search.result == "null"
+      @projects = Project.all
+    else
+      @projects = @search.result
     end
+  end
 
-    def show
-    end
-    
-    def new
-        @project = Project.new
-    end
+  def show
+  end
 
-    def create
-      @project = Project.new(project_params)
-      if @project.save
-        redirect_to projects_path
-      else
-        render 'new'
-      end
-    end
+  def new
+    @project = Project.new
+  end
 
-    def edit
-    end
-
-    def update
-      if @project.update(project_params)
-        redirect_to projects_path
-      else
-        render 'edit'
-      end
-    end
-
-    def destroy
-      @project.destroy
+  def create
+    @project = Project.new(project_params)
+    if @project.save
       redirect_to projects_path
+    else
+      render :new
     end
-    private
+  end
 
-    def set_project
-      @project = Project.find(params[:id])
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+      redirect_to projects_path
+    else
+      render :edit
     end
+  end
 
-    before_action :set_project, only: [:show, :edit, :update, :destroy]
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+  private
+
+  def set_project
+    @project = Project.find(params[:id])
+  end
+
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
 
 
-    def project_params
-      params[:project].permit(:title)
-    end
+  def project_params
+    params[:project].permit(:title)
+  end
 end
